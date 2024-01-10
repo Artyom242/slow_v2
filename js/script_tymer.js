c998d63d11ab0dd56181d3179e949c661feb3572
Kot__Mika <97839696+Artyom242@users.noreply.github.com>
2024-01-10 21:41:03 +0700

    Update script_tymer.js

--- js/script_tymer.js
+++ js/script_tymer.js
@@ -15,6 +15,6 @@ var x = setInterval(function () {
 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("demo").innerHTML = "С НОВЫМ ГОДОМ!!!";
     }
 }, 1000);
